bc127d2be80a023c419b19cf8de8da31370c7092
Abderrahmane Ouaday <abdoouaday361@gmail.com>
2024-12-03 05:04:04 +0100

    add elt pipeline

---
+++ spark-scripts/load.py
@@ -0,0 +1,44 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col, to_timestamp, sum, round, expr
+
+# Create a SparkSession
+spark = SparkSession.builder \
+    .appName("E-Commerce Data Transformation") \
+    .config("spark.sql.catalogImplementation", "hive") \
+    .config("hive.metastore.uris", "thrift://hive-metastore:9083") \
+    .config("spark.sql.warehouse.dir", "hdfs://namenode:9000/user/hive/warehouse") \
+    .enableHiveSupport() \
+    .getOrCreate()
+
+    
+# Step 1: Create a database if it doesn't already exist
+spark.sql("CREATE DATABASE IF NOT EXISTS ecommerce_db")
+
+# Step 2: Switch to the newly created (or existing) database
+spark.sql("USE ecommerce_db")
+
+# Step 3: Create the `ecommerce_data` table with appropriate schema
+# The table will store data in TEXTFILE format and skip the header line.
+spark.sql("""
+    CREATE TABLE IF NOT EXISTS ecommerce_raw (
+        InvoiceNo STRING,
+        StockCode STRING,
+        Description STRING,
+        Quantity INT,
+        InvoiceDate STRING,
+        UnitPrice FLOAT,
+        CustomerID STRING,
+        Country STRING
+    )
+    ROW FORMAT DELIMITED
+    FIELDS TERMINATED BY ','  # Columns are separated by commas
+    STORED AS TEXTFILE  # Data is stored as a text file
+    TBLPROPERTIES ("skip.header.line.count"="1")  # Skip the header line during data load
+""")
+
+# Step 4: Load data from HDFS into the `ecommerce_data` table
+# Ensure that the file path '/input/ecommerce_data/data.csv' exists in HDFS.
+spark.sql("LOAD DATA INPATH '/input/ecommerce_data/data.csv' INTO TABLE ecommerce_raw")
+
+# Stop the Spark session
+spark.stop()
